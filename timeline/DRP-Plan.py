697afffcd5ad15ea812364862bf5f6c6a1b8f192
Hassan Siddiqui <hassans@astro.princeton.edu>
2020-06-08 16:05:47 -0400

    version of plan emailed to PU

--- timeline/DRP-Plan.py
+++ timeline/DRP-Plan.py
@@ -22,7 +22,7 @@ catColors = dict(Project='rgb(255, 51, 0)',
                  Infra='rgb(153, 0, 153)')
 
 
-# In[143]:
+# In[151]:
 
 
 df = pd.read_csv('drp-plan.csv').applymap(lambda x: x.strip() if type(x)==str else x)
