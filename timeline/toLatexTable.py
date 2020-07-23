4a0a5599af9e29b49dc85f33d48c01c9a23ed90e
Hassan Siddiqui <hassans@astro.princeton.edu>
2020-07-23 07:38:26 -0400

    summarised resource list in latex table

--- timeline/toLatexTable.py
+++ timeline/toLatexTable.py
@@ -3,18 +3,31 @@
 
 import pandas as pd
 
+def process_resources(row):
+    resources = ""
+    for name in ['Price', 'Caplar', 'Belland', 'Yasuda', 'Yabe', 'Yamashita']:
+        if row[name] > 0:
+            resources += f'{name}:{row[name]}, '
+    return resources[:-2]
+
+
 # Load in CSV
 # Last row is usually plotted first, so reverse that.
-df = pd.read_csv('drp-plan.csv').applymap(lambda x: x.strip() if type(x)==str else x).iloc[::-1]
+df0 = pd.read_csv('drp-plan.csv').applymap(lambda x: x.strip() if type(x)==str else x).iloc[::-1]
 
-df_report0 = df[['Task', 'Start', 'Finish', 'Category', 'Description', 'Price', 'Caplar', 'Belland', 'Yasuda', 'Yabe', 'Yamashita']].copy()
+df1 = df0[['Task', 'Start', 'Finish', 'Category', 'Description', 'Price', 'Caplar', 'Belland', 'Yasuda', 'Yabe', 'Yamashita']].copy()
 
 # Remove Project and Hardware tasks
 
-hp_indices = df[(df['Category']=='Hardware') | (df['Category']=='Project')].index
-df_report = df.drop(hp_indices)
+hp_indices = df1[(df1['Category']=='Hardware') | (df1['Category']=='Project')].index
+df2 = df1.drop(hp_indices)
+
+for index, row in df2.iterrows():
+    df2.at[index, 'Resource'] = process_resources(row)
+
+df3 = df2[['Task', 'Start', 'Finish', 'Description', 'Resource']].copy()
 
 with open("doc/table.tex", "w") as text_file:
-    text_file.write(df_report.to_latex(index=False, longtable=True, column_format='llllp{2cm}llllll'))
+    text_file.write(df3.to_latex(index=False, longtable=True, column_format='lllp{2cm}l'))
 
 
